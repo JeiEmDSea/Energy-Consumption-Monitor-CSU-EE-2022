fab4228687e99f424524c0fad26b51d93cfeb580
John Mark Centeno <jeiem.centeno@gmail.com>
2022-09-04 08:53:16 +0800

    Added graphing code with working OLED display

--- arduino/src/energy-consupmtion-monitor/energy-consupmtion-monitor.ino
+++ arduino/src/energy-consupmtion-monitor/energy-consupmtion-monitor.ino
@@ -5,7 +5,9 @@
 #include "Sim800L.h"
 #include "SoftwareSerial.h"
 #include "SdFat.h"
-#include "ArduinoJson.h"
+#include "Arduino.h"
+#include "U8g2lib.h"
+#include "Wire.h"
 
 #define VOLTAGE_SENSOR_PIN A0
 #define CURRENT_SENSOR_PIN A1
@@ -20,13 +22,21 @@ double pricePerkWh = 10;
 
 char num[] = "+639355276219";
 
+bool redrawGraph = true;
+double ox, oy;
+double lastReading = 0;
+double yLength = 15;
+double x = 0;
+double xLength = 60;
+double xInterval = 20;
+
 EnergyMonitor emon;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 DS3231 rtc(SDA, SCL);
 SdFat sdCard;
 SdFile sdFile;
 Sim800L gsm(RX, TX);
-SoftwareSerial dataPlotter(2, 3);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, SCL, SDA);
 
 void setup()
 {
@@ -37,8 +47,11 @@ void setup()
   initSDcard();
   rtc.begin();
   gsm.begin(9600);
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
 
   loadCurrentConsumption();
+  lastReading = energy_kWh * 1000;
 }
 
 void loop()
@@ -46,17 +59,32 @@ void loop()
   calculateValues();
   showValuesToLCD();
   saveDataToSDcard();
-  sendDataToUser(17, 0); // ? every 5:00PM
+  // sendData(17, 0); // ? every 5:00PM
+
+  drawGraph(
+      x++,                   // second
+      energy_kWh * 1000,     // consumption
+      30,                    // x origin
+      50,                    // y origin
+      75,                    // graph width
+      30,                    // graph height
+      0,                     // start second
+      xLength,               // end second
+      xInterval,             // x-axis division
+      lastReading,           // y-axis lower bound
+      lastReading + yLength, // y-axis upper bound
+      lastReading + yLength, // y-axis division
+      0,
+      redrawGraph);
+
+  if (x == xLength)
+  {
+    x = 0;
+    lastReading = energy_kWh * 1000;
+    redrawGraph = true;
+  }
 
   delay(1000);
-}
-
-void sendDataToPlotter()
-{
-  StaticJsonDocument<100> sensorData;
-
-  sensorData["energy_kWh"] = energy_kWh;
-  dataPlotter.print(sensorData.as<String>());
 }
 
 void calculateValues()
@@ -156,10 +184,10 @@ void loadCurrentConsumption()
   }
 }
 
-void sendDataToUser(int hr, int min)
+void sendData(int hr, int min)
 {
   if (rtc.getTime().hour == hr && rtc.getTime().min == min && rtc.getTime().sec == 0)
-  // ! if (rtc.getTime().min % 10 == 0) // ? every 10 minutes
+  // if (rtc.getTime().min % 10 == 0) // ? every 10 minutes
   {
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -191,28 +219,6 @@ void initSDcard()
     lcd.print("SD Card initalized");
     delay(2000);
   }
-
-  /** Connection error handling
-   * *else
-   * *{
-   * *  lcd.clear();
-   * *  lcd.setCursor(0, 0);
-   * *  lcd.print("SD card initialization failed");
-   * *  delay(2000);
-   * *  return;
-   * *}
-   * *
-   * *myFile = SD.open("DATA.txt", FILE_WRITE);
-   * *
-   * *if (myFile)
-   * *{
-   * *  lcd.clear();
-   * *  lcd.setCursor(0, 0);
-   * *  lcd.print("Data file ready");
-   * *  delay(2000);
-   * *  myFile.close();
-   * *}
-   */
 }
 
 String mapByteToString(int byte)
@@ -242,4 +248,68 @@ String mapByteToString(int byte)
   default:
     return ".";
   }
+}
+
+void drawGraph(double x, double y, double gx, double gy, double w, double h, double xlo, double xhi, double xinc, double ylo, double yhi, double yinc, double dig, boolean &redraw)
+{
+  double i = 0;
+  double temp = 0;
+
+  if (redraw == true)
+  {
+    redraw = false;
+    u8g2.clearBuffer();
+
+    ox = (x - xlo) * (w) / (xhi - xlo) + gx;
+    oy = (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+
+    // draw y scale
+    for (i = ylo; i <= yhi; i += yinc)
+    {
+      // compute the transform
+      // note my transform funcition is the same as the map function, except the map uses long and we need doubles
+      temp = (i - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+      if (i == 0)
+      {
+        u8g2.drawHLine(gx - 3, temp, w + 3);
+      }
+      else
+      {
+        u8g2.drawHLine(gx - 3, temp, 3);
+      }
+      u8g2.setCursor(gx - 27, temp - 3);
+      u8g2.println(i, dig);
+    }
+
+    // draw x scale
+    for (i = xlo; i <= xhi; i += xinc)
+    {
+      // compute the transform
+      temp = (i - xlo) * (w) / (xhi - xlo) + gx;
+      if (i == 0)
+      {
+        u8g2.drawVLine(temp, gy - h, h + 3);
+      }
+      else
+      {
+        u8g2.drawVLine(temp, gy, 3);
+      }
+      u8g2.setCursor(temp, gy + 6);
+      u8g2.println(i, dig);
+    }
+  }
+
+  // graph drawn now plot the data
+  // the entire plotting code are these few lines...
+
+  x = (x - xlo) * (w) / (xhi - xlo) + gx;
+  y = (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+  u8g2.drawLine(ox, oy, x, y);
+  u8g2.drawLine(ox, oy - 1, x, y - 1);
+  ox = x;
+  oy = y;
+
+  // up until now print sends data to a video buffer NOT the screen
+  // this call sends the data to the screen
+  u8g2.sendBuffer();
 }
